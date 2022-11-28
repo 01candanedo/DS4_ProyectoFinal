d20810748ebdab9af4743bbedb361a1620a3ec5e
MarckJrquin <marck0208@outlook.com>
2022-11-27 20:13:05 -0500

    .

--- FrontendOlimpicos/view/js/ControllersJS/NoticiasController.js
+++ FrontendOlimpicos/view/js/ControllersJS/NoticiasController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:FrontendOlimpicos/view/js/NoticiasController.js
 let datos = [];
 
 function ObtenerNoticias(){
@@ -37,44 +36,4 @@ function MapearPlantilla(datos){
         </div>
     `
 
-=======
-let datos = [];
-
-function ObtenerNoticias(){
-
-    fetch("http://localhost:8080/noticias/all").
-    then(resultado => {
-        resultado.json().then(json => {
-            datos = json;
-            console.log(datos);
-            MapearDatos();
-        })
-    });
-}
-
-function MapearDatos(){
-    //let contenido = document.getElementById('contenido');
-    let contenedor = document.getElementById('news-container');
-    for(let i=0; i<datos.length; i++){
-        contenedor.innerHTML += MapearPlantilla(datos[i]);
-    }
-}
-
-function MapearPlantilla(datos){
-    return `
-        <div class="news-card">
-            <div class="news-image">
-                <img src="${datos.imagen}" alt="">
-                <a href="${datos.enlace}" target="_blank">
-                    <button class="card-btn">ver noticia completa</button>
-                </a>
-            </div>
-            <div class="news-info">
-                <h2 class="news-title">${datos.titulo}</h2>
-                <p class="news-description">"${datos.descripcion}"</p>
-            </div>
-        </div>
-    `
-
->>>>>>> 2a1d71dafab490173fca79a4d2338e96c9c5a61e:FrontendOlimpicos/view/js/ControllersJS/NoticiasController.js
 }
