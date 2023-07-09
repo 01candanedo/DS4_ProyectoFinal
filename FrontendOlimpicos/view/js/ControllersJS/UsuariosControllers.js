e1f6e41243ea80426549823d1787ced4d021a8a2
irving_javier15 <bbjavier09@hotmail.com>
2023-07-09 18:20:24 -0500

    expresion regular para validar contraeña y limite de intentos fallidos

--- FrontendOlimpicos/view/js/ControllersJS/UsuariosControllers.js
+++ FrontendOlimpicos/view/js/ControllersJS/UsuariosControllers.js
@@ -1,6 +1,9 @@
 let baseUrl = "http://localhost:8080";
+let contadorIncorrectos = 0; 
+let bloquearInicioSesion = false;
 let usuarios = [];
 let perfiles = [];
+
 
 function ObtenerUsuarios() {
     fetch(baseUrl + '/usuarios/all').then(res => {
@@ -19,28 +22,45 @@ function ObtenerUsuariosPerfil() {
     });
 }
 
-function VerificarEmail(){
-    let flag=false;
-    let checkemail = document.getElementById("email_login").value;
-    let checkpass = document.getElementById("contrasenia_login").value;
-    let email, pass, id;
-    usuarios.forEach(usr => {
-        if(usr.email === checkemail && usr.pass === checkpass){
-            flag=true;
-            email = usr.email;
-            pass = usr.pass;
-            id = usr.id;
-        }
-    });
-    if(flag){
-        if(email === checkemail && pass === checkpass){
-            CrearSesion(id);
-            window.location.replace("../../../index.html");
-        }
-    }else{
-        alert("Datos incorrectos.., Intentelo nuevamente")
-    }
+function VerificarEmail() {
+  if (bloquearInicioSesion) {
+    console.log("Debes esperar 30 segundos antes de volver a intentar iniciar sesión.");
+    return;
+  }
 
+  let flag = false;
+  let checkemail = document.getElementById("email_login").value;
+  let checkpass = document.getElementById("contrasenia_login").value;
+  let email, pass, id;
+
+  usuarios.forEach(usr => {
+    if (usr.email === checkemail && usr.pass === checkpass) {
+      flag = true;
+      email = usr.email;
+      pass = usr.pass;
+      id = usr.id;
+    }
+  });
+
+  if (flag) {
+    if (email === checkemail && pass === checkpass) {
+      CrearSesion(id);
+      window.location.replace("../../../index.html");
+    }
+  } else {
+    alert("Datos incorrectos. Inténtelo nuevamente");
+    contadorIncorrectos++;
+    if (contadorIncorrectos >= 3) {
+      alert("Límite de intentos fallidos alcanzado. Esperando 30 segundos...");
+      bloquearInicioSesion = true;
+
+      setTimeout(function() {
+        alert("Se ha restablecido el inicio de sesión.");
+        bloquearInicioSesion = false;
+        contadorIncorrectos = 0;
+      }, 30000);
+    }
+  }
 }
 
 function VerificarDatos(){
@@ -95,35 +115,50 @@ function CalcularFecha(){
 }
 
 function GuardarUsuario() {
-    let usr = {
-        usuario: document.getElementById("usuario_registro").value,
-        nombre: document.getElementById("nombre_registro").value,
-        apellido: document.getElementById("apellido_registro").value,
-        email: document.getElementById("email_registro").value,
-        pass: document.getElementById("contrasenia_registro").value,
-        foto: "Default",
-        fecha_Creacion: CalcularFecha()
-    };
+    let validPass = false;
+    let regexPass = /^(?=.*[A-Z])(?=.*\d)(?=.*\W).+$/;
+    let pass = document.getElementById("contrasenia_registro").value;
+    
+    if (regexPass.test(pass)) {
+        validPass = true;   
+    }
 
-    fetch(baseUrl + "/usuario", {
-        method: "POST",
-        body: JSON.stringify(usr),
-        headers: {
-            "Content-type": 'application/json; charset=UTF-8'
-        }
-    }).then(
-        response => {
-            document.getElementById("usuario_registro").value = "";
-            document.getElementById("nombre_registro").value = "";
-            document.getElementById("apellido_registro").value = "";
-            document.getElementById("email_registro").value = "";
-            document.getElementById("contrasenia_registro").value = "";
-            alert("Registro Exitoso")
-        }
-    ).catch(
-        error => {
-            alert("Ocurrio un error")
-        });
+    if (validPass) {
+        let usr = {
+            usuario: document.getElementById("usuario_registro").value,
+            nombre: document.getElementById("nombre_registro").value,
+            apellido: document.getElementById("apellido_registro").value,
+            email: document.getElementById("email_registro").value,
+            pass: pass,
+            foto: "Default",
+            fecha_Creacion: CalcularFecha()
+        }; 
+
+        alert("Usuario registrado con éxito");
+
+        fetch(baseUrl + "/usuario", {
+            method: "POST",
+            body: JSON.stringify(usr),
+            headers: {
+                "Content-type": 'application/json; charset=UTF-8'
+            }
+        }).then(
+            response => {
+                document.getElementById("usuario_registro").value = "";
+                document.getElementById("nombre_registro").value = "";
+                document.getElementById("apellido_registro").value = "";
+                document.getElementById("email_registro").value = "";
+                document.getElementById("contrasenia_registro").value = "";
+                
+            }
+        ).catch(
+            error => {
+                alert("Ocurrió un error");
+            }
+        );
+    } else {
+        alert("La contraseña debe contener al menos una mayúscula, números y un caracter especial mi bro");
+    }
 }
 
 
